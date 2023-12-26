cedfbd8ca10b9a699e5958282b30fc71f9347fab
theomorphic <FelixMK@yandex.ru>
2023-12-26 05:08:06 +0500

    feat: add orange button

---
+++ script.js
@@ -0,0 +1,72 @@
+
+orange_btn.addEventListener('click', () => {
+
+
+	// document.getElementById("box").classList.remove("box")
+	// document.getElementById("box1").style.border = null;
+
+	// document.getElementById("box").classList.add("active_orange")
+
+	const orange1 = document.createElement("img");
+	orange1.src = "./img/orange-small.png";
+	let src1 = document.getElementById("box1");
+	src1.appendChild(orange1);
+	document.getElementById("box1").classList.remove("box1");
+
+	const orange2 = document.createElement("img");
+	orange2.src = "./img/orange-small.png";
+	let src2 = document.getElementById("box2");
+	src2.appendChild(orange2);
+	document.getElementById("box2").classList.remove("box2");
+
+	const orange3 = document.createElement("img");
+	orange3.src = "./img/orange-small.png";
+	let src3 = document.getElementById("box3");
+	src3.appendChild(orange3);
+	document.getElementById("box3").classList.remove("box3");
+
+	const orange4 = document.createElement("img");
+	orange4.src = "./img/orange-small.png";
+	let src4 = document.getElementById("box4");
+	src4.appendChild(orange4);
+	document.getElementById("box4").classList.remove("box4");
+
+	const orange5 = document.createElement("img");
+	orange5.src = "./img/orange-small.png";
+	let src5 = document.getElementById("box5");
+	src5.appendChild(orange5);
+	document.getElementById("box5").classList.remove("box5");
+
+	const orange6 = document.createElement("img");
+	orange6.src = "./img/orange-small.png";
+	let src6 = document.getElementById("box6");
+	src6.appendChild(orange6);
+	document.getElementById("box6").classList.remove("box6");
+
+	const orange7 = document.createElement("img");
+	orange7.src = "./img/orange-small.png";
+	let src7 = document.getElementById("box7");
+	src7.appendChild(orange7);
+	document.getElementById("box7").classList.remove("box7");
+
+	const orange8 = document.createElement("img");
+	orange8.src = "./img/orange-small.png";
+	let src8 = document.getElementById("box8");
+	src8.appendChild(orange8);
+	document.getElementById("box8").classList.remove("box8");
+
+	const orange9 = document.createElement("img");
+	orange9.src = "./img/orange-small.png";
+	let src9 = document.getElementById("box9");
+	src9.appendChild(orange9);
+	document.getElementById("box9").classList.remove("box9");
+
+	const orange10 = document.createElement("img");
+	orange10.src = "./img/orange-small.png";
+	let src10 = document.getElementById("box10");
+	src10.appendChild(orange10);
+	document.getElementById("box10").classList.remove("box10");
+
+
+	// alert('Orange activated')
+      })
